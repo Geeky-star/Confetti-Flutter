a856b1ae909d5d9afe75f0f25dc0d86706519072
Geeky-star <nimratkaur540@gmail.com>
2022-01-06 14:34:23 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:confetti/confetti.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //declare confettiController;
+  late ConfettiController _topController;
+
+  @override
+  void initState() {
+    super.initState();
+    //initialize confettiController
+    _topController = ConfettiController(duration: const Duration(seconds: 10));
+  }
+
+  @override
+  void dispose() {
+    //dispose the controller
+    _topController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("GeeksForGeeks"),
+          backgroundColor: Colors.green,
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              //align the confetti on the screen
+              Align(
+                alignment:
+                    Alignment.topCenter, //confetti will pop from top-center
+                child: ConfettiWidget(
+                  confettiController: _topController,
+                  blastDirection: pi / 2,
+                  maxBlastForce: 5,
+                  minBlastForce: 1,
+                  emissionFrequency: 0.01,
+                  numberOfParticles: 20, //10 paticles will pop-up at a time
+                  gravity: 1, //particles will come down
+                  shouldLoop:
+                      true, // start again as soon as the animation is finished
+                  colors: const [
+                    Colors.green,
+                    Colors.yellow,
+                    Colors.pink,
+                    Colors.orange,
+                    Colors.blue
+                  ], //assign colors of any choice
+                ),
+              ),
+              Center(
+                child: TextButton(
+                    onPressed: () {
+                      //invoking confettiController to come into play
+                      _topController.play();
+                    },
+                    child: Text('Top',
+                        style: const TextStyle(
+                            color: Colors.white, fontSize: 20))),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
